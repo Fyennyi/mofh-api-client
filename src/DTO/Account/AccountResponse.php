641c3fe9413facce4269e83a7e042f35df4e1c8c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-09 07:41:44 +0200

    feat: Add account response DTO

---
+++ src/DTO/Account/AccountResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Fyennyi\MofhApi\DTO\Account;
+
+final readonly class AccountResponse
+{
+    public function __construct(
+        public string $vPanelUsername,
+        public string $statusMessage,
+        public array $nameservers
+    ) {}
+
+    public static function fromXml(\SimpleXMLElement $xml): self
+    {
+        $options = $xml->result->options;
+        $ns = [];
+        if (isset($options->nameserver)) $ns[] = (string)$options->nameserver;
+        if (isset($options->nameserver2)) $ns[] = (string)$options->nameserver2;
+
+        return new self(
+            vPanelUsername: (string)$options->vpusername,
+            statusMessage: (string)$xml->result->statusmsg,
+            nameservers: $ns
+        );
+    }
+}

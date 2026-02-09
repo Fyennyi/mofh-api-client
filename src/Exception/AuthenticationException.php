64b522326f1f4c9553dcffc94a1cab56a3feb331
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-09 09:21:30 +0200

    feat: Add authentication exception

---
+++ src/Exception/AuthenticationException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Fyennyi\MofhApi\Exception;
+
+/**
+ * Thrown when API credentials (username/API key) are invalid.
+ */
+final class AuthenticationException extends MofhException {}

22ece54a7ba1dc7bd332a6d18ecf7d145e6b5b92
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-09 09:22:20 +0200

    feat: Add validation exception

---
+++ src/Exception/ValidationException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Fyennyi\MofhApi\Exception;
+
+/**
+ * Thrown when request data fails validation before sending to API.
+ */
+final class ValidationException extends MofhException {}
